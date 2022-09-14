47134f5269357f7db4365fc050eb2aab889ae67d
ProstoSasha0612 <alexandr.popko.2013@mail.ru>
2022-09-14 22:16:20 +0300

    unwork: viewpager in fragment

--- app/src/main/java/com/projectapp/moviesapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/projectapp/moviesapp/presentation/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.projectapp.moviesapp.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.projectapp.moviesapp.R
 import com.projectapp.moviesapp.databinding.ActivityMainBinding
+import com.projectapp.moviesapp.domain.usecases.movielist.MovieType
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +23,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun openMoviesListFragment(){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, FragmentMoviesList.newInstance())
-            .commit()
+//    private fun openMoviesListFragment(){
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container,FragmentMoviesListPager.newInstance())
+//            .addToBackStack("ViewPagerFragment")
+////            .replace(R.id.fragment_container, FragmentMoviesList.newInstance(MovieType.POPULAR))
+//            .commit()
+//
+//    }
 
+    private fun openMoviesListFragment(){
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.action_navHostFragment_to_fragmentMoviesListPager)
     }
 }
